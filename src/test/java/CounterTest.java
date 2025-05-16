3454895955886712d1eb9bc9a2650bc07d78266a
fridoa <fridoafriyanto3@gmail.com>
2025-05-16 21:21:07 +0700

    first commit

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -4,28 +4,26 @@ import static org.junit.Assert.*;
 import src.main.java.*;
 import org.junit.Test;
 
-import Counter;
-
 public class CounterTest {
 
 	@Test
 	public void testReset() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 0; i <10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			testCounter.increment();
 		}
-		
+
 		testCounter.reset();
-		
+
 		assertEquals(testCounter.getCount(), 0);
 	}
 
 	@Test
 	public void testIncrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.increment();
 			assertEquals(testCounter.getCount(), i);
 		}
@@ -34,8 +32,8 @@ public class CounterTest {
 	@Test
 	public void testDecrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.decrement();
 			assertEquals(testCounter.getCount(), i * -1);
 		}
